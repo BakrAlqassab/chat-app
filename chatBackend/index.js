8cc8a10025e39100295a5cef5257f66f2997aaaa
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-01-09 15:50:51 +0200

    prepare expressjs framework, and add variables and install dotenv

---
+++ chatBackend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require('dotenv').config();
+const app = express();
+const port = process.env.APP_PORT;
+app.get("/home", (req, res) => {
+  return res.send("Home Screen");
+});
+
+app.get("/login", (req, res) => {
+  return res.send("Login UI");
+});
+
+app.listen(port, () => {
+  console.log(`server listening for port number : ${port}`);
+});
